bcf89f0930bd7f8a1296eeb9a6b41817392df28c
Awesomeyaya <44456090+Awesomeyaya@users.noreply.github.com>
2018-10-27 18:43:42 -0400

    Update two-sum.py

--- Two-pointer/two-sum.py
+++ Two-pointer/two-sum.py
@@ -1,1 +1,16 @@
-def sum(self, int a):
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(nums) < 2:
+            return False
+        dir1 = {}
+        for i in range(len(nums)):
+            if target - nums[i] in dir1:
+                return [dir1[target - nums[i]] , i]
+            else :
+                dir1[nums[i]] =i
+                
