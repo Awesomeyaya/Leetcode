438fa5bc7bfe3c056c6e700137c4e073ef108095
Awesomeyaya <44456090+Awesomeyaya@users.noreply.github.com>
2018-10-27 19:17:50 -0400

    Create Remove Duplicates from Sorted Array #26.py

---
+++ Two-pointer/Move elements in the array/Remove Duplicates from Sorted Array #26.py
@@ -0,0 +1,15 @@
+def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if not nums:
+            return 0 
+        
+        pointer = 0
+        for n in range(1,len(nums)) :
+            if nums[n] != nums[pointer]:
+                pointer = pointer + 1
+                nums[pointer] = nums[n]
+        return pointer + 1
