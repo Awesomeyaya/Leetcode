133d29e69473d449f25283735cb50ed541fad357
Awesomeyaya <44456090+Awesomeyaya@users.noreply.github.com>
2018-10-27 18:48:53 -0400

    Update and rename two sum II.py to two sum II - Input is a sorted array.py

---
+++ Two-pointer/two sum II - Input is a sorted array.py
@@ -0,0 +1,19 @@
+ def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start = 0
+        end = len(numbers) - 1
+        list = []
+        while start <= end: 
+            if numbers[start] + numbers[end] ==  target:
+                break
+            elif numbers[start] + numbers[end] > target:
+                end = end - 1 
+            elif numbers[start] + numbers[end] < target:
+                start = start + 1
+        list.append(start+1)
+        list.append(end+1)
+        return list
--- Two-pointer/two sum II.py
+++
@@ -1,1 +0,0 @@
-def twoSum(self, numbers, target): 
