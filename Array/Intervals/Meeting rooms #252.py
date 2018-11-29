307bbb6149fb5df000164752b39833e607922bae
Awesomeyaya <44456090+Awesomeyaya@users.noreply.github.com>
2018-11-29 17:09:50 -0500

    Update Meeting rooms #252.py

--- Array/Intervals/Meeting rooms #252.py
+++ Array/Intervals/Meeting rooms #252.py
@@ -1,3 +1,17 @@
+'''
+Problem Description:
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+Example 1:
+
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: true
+
+'''
 # Definition for an interval.
 # class Interval:
 #     def __init__(self, s=0, e=0):
