e006a9e50c456ef83107ff4402adbc5a32dfa706
Awesomeyaya <44456090+Awesomeyaya@users.noreply.github.com>
2019-01-07 18:02:17 -0500

    Update BFS.py

--- BFS and DFS/BFS.py
+++ BFS and DFS/BFS.py
@@ -1,7 +1,7 @@
 Usually, a problem can be solved with both DFS and BFS.
 BFS focus on queue. 
 
-For example, for the island problem. BFS can find the area, perimeter and position of the island.
+For example, for the island problem. BFS can find the area, perimeter of the island, also the shortese bridge between two islands.
 
 1.Find the perimeter of an island
 def sum(i,j):
@@ -12,7 +12,25 @@ def sum(i,j):
             res +=1
     return res 
     
-2.Find the area of an island 
+2.Find the shortest bridge 
+... Firstly we use DFS to find the positions of an island using visited matrix. 
+Then we get a queue q with the positions of the island. 
+dist = 0
+while not q.empty():
+    size = q.qsize()
+    for i in range(size):
+        r,l = q.get()
+        adjacent = (r-1,l),(r+1,l),(r,l-1),(r,l+1),
+        for x,y in adjacent:
+            if 0<=x<m and 0<=y<n and visited[x][y] == False:
+                if A[x][y] == 1:
+                    return dist
+                else:
+                    q.put((x,y))
+                    visited[x][y] = True
+        dist += 1
+return -1
+
 
         
     
