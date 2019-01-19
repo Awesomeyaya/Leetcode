7a0c415cfda5bcfa519449418b4b2cd2eb58dabb
Awesomeyaya <44456090+Awesomeyaya@users.noreply.github.com>
2019-01-18 20:43:43 -0500

    Update Partition.py

--- Two-pointer/Sorting/Partition.py
+++ Two-pointer/Sorting/Partition.py
@@ -1,7 +1,23 @@
 '''
-Partition tempelet
+Partition tempelet: O(n)
+pivot 左边 <pivot 
+pivot 右边 >= pivot
 '''
 
 def Partition(self,nums,start,end):
 if start >= end:
+    return 
+mid = (start+end)//2 
+left, right, pivot = start, end, nums[mid]
+while left <= right:
+    while left <= right and nums[left] < pivot:
+            left += 1 
+        while left <= right and nums[right] >= pivot:
+            right -= 1 
+        if left <= right:
+            nums[left],nums[right] = nums[right],nums[left]
+            left += 1 
+            right -=1 
+            
+      
     
